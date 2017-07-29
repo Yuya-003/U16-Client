02a4d34dcadfc039a68347fa2847df3f58cfef1b
Yuya-003 <yu1ohmi@gmail.com>
2017-07-29 16:41:29 +0900

    ループになったら抜け出せないくそざこなめくじができた

--- U16-Client/main.cpp
+++ U16-Client/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include "./client/Client.h"
 
+//nothing:0, other player:1, block:2, item:3
 
 int main(int argc, char* argv[]) {
     std::cout   << " **************************************\n"
@@ -14,9 +15,11 @@ int main(int argc, char* argv[]) {
     Dir walk_dir = up;
     Dir item_dir = off;
 
+    //以下、ゲームのループ
     while (getReady(value_ready)) {
         
-        if (value_ready[up] == 1) {     //敵をつぶすところ
+        //敵をつぶすところ
+        if (value_ready[up] == 1) {
             putUp(value_method);
         }
         else if (value_ready[left] == 1) {
@@ -28,46 +31,81 @@ int main(int argc, char* argv[]) {
         else if (value_ready[down] == 1) {
             putDown(value_method);
         }
-        else {                          //アイテムを取るところ
+        else {
+
+            //アイテムを取るところ
             if (item_dir != off) {
-
+                walk_dir = item_dir;
+                walk(item_dir, value_method);
+                item_dir = off;
             }
+            else if (value_ready[up] == 3) {
+                item_dir = up;
+                lookUp(value_method);
+                if (value_method[up] == 2 && value_method[left] == 2 && value_method[right] == 2) {
+                    item_dir = off;
+                }
+            }
+            else if (value_ready[left] == 3) {
+                item_dir = left;
+                lookLeft(value_method);
+                if (value_method[up] == 2 && value_method[left] == 2 && value_method[down] == 2) {
+                    item_dir = off;
+                }
+            }
+            else if (value_ready[right] == 3) {
+                item_dir = right;
+                lookRight(value_method);
+                if (value_method[up] == 2 && value_method[right] == 2 && value_method[down] == 2) {
+                    item_dir = off;
+                }
+            }
+            else if (value_ready[down] == 3) {
+                item_dir = down;
+                lookDown(value_method);
+                if (value_method[left] == 2 && value_method[right] == 2 && value_method[down] == 2) {
+                    item_dir = off;
+                }
+            }
+            else {
 
-            while (true) {
-                if (walk_dir == up) {
-                    if (value_ready[up] != 2) {
-                        walkUp(value_method);
-                        break;
+                //動くところ
+                while (true) {
+                    if (walk_dir == up) {
+                        if (value_ready[up] != 2) {
+                            walkUp(value_method);
+                            break;
+                        }
+                        else {
+                            walk_dir = left;
+                        }
                     }
-                    else {
-                        walk_dir = left;
+                    if (walk_dir == left) {
+                        if (value_ready[left] != 2) {
+                            walkLeft(value_method);
+                            break;
+                        }
+                        else {
+                            walk_dir = down;
+                        }
                     }
-                }
-                if (walk_dir == left) {
-                    if (value_ready[left] != 2) {
-                        walkLeft(value_method);
-                        break;
+                    if (walk_dir == down) {
+                        if (value_ready[down] != 2) {
+                            walkDown(value_method);
+                            break;
+                        }
+                        else {
+                            walk_dir = right;
+                        }
                     }
-                    else {
-                        walk_dir = down;
-                    }
-                }
-                if (walk_dir == down) {
-                    if (value_ready[down] != 2) {
-                        walkDown(value_method);
-                        break;
-                    }
-                    else {
-                        walk_dir = right;
-                    }
-                }
-                if (walk_dir == right) {
-                    if (value_ready[right] != 2) {
-                        walkRight(value_method);
-                        break;
-                    }
-                    else {
-                        walk_dir = up;
+                    if (walk_dir == right) {
+                        if (value_ready[right] != 2) {
+                            walkRight(value_method);
+                            break;
+                        }
+                        else {
+                            walk_dir = up;
+                        }
                     }
                 }
             }
@@ -78,6 +116,8 @@ int main(int argc, char* argv[]) {
 
 
     }
+    //ここまでゲームのループ
+
     std::cout << "<<----ゲーム終了---->>" << std::endl;
 
     return 0;
